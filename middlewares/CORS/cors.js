3a1598061ad47899ed4ec10c83f651b436060e50
David Angeles Ungson <davidangelesu@gmail.com>
2020-10-26 23:08:46 +0100

    Fixed CORS error

--- middlewares/CORS/cors.js
+++ middlewares/CORS/cors.js
@@ -1,11 +1,4 @@
 //** Middleware for preventing CORS errors  */
+const cors = require("cors");
 
-module.exports = (req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "*");
-	if (req.methods === "OPTIONS") {
-		res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-		return res.status(200).json({});
-	}
-	next();
-};
+module.exports = cors();
