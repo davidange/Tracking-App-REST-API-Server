54fdd6605e3ebdc2e3eb04d90c4fbca089c38314
David Angeles Ungson <davidangelesu@gmail.com>
2020-10-26 23:03:04 +0100

    CORS Error

--- middlewares/CORS/cors.js
+++ middlewares/CORS/cors.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "*");
 	if (req.methods === "OPTIONS") {
-		res.header("Access-Control-Allow-Methods", "PUT, OPTIONS, POST, DELETE, GET");
+		res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		return res.status(200).json({});
 	}
 	next();
